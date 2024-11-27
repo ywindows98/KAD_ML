c6112bd68085f64b160ea3ce36437a74cf49a2f7
ywindows98 <124464870+ywindows98@users.noreply.github.com>
2024-11-27 19:56:53 +0100

    Upload actualized version with TODO

--- KAD_prototype1.py
+++ KAD_prototype1.py
@@ -22,31 +22,39 @@ shrooms_classes = shrooms['class']
 # generating possible combinations of attributes
 combinations = get_combinations(dataframe=shrooms_attributes, length=3)
 print(f"Generated {len(combinations)} combinations")
-print(combinations)
+# print(combinations)
 
 n_records = len(shrooms)
 
 # counting frequences of combination for two classes
 edible_counts, poisonous_counts = count_combinations_per_class(combinations, shrooms_attributes, shrooms_classes)
 combination_frequences = np.add(edible_counts, poisonous_counts)
-print(edible_counts)
-print(poisonous_counts)
-print(combination_frequences)
+# print(edible_counts)
+# print(poisonous_counts)
+# print(combination_frequences)
 
 # poziadovana fundovanost kombinacie
 # requested combination reliability
 rcr = 0.2
 
 # finding reliable combinations based on the rcr
-reliable_indexes, reliable_combinations = get_reliable_combinations(combinations, combination_frequences, rcr, n_records)
+reliable_indexes, reliable_combinations = get_reliable_combinations(combinations, combination_frequences, rcr,
+n_records)
 
 # print(reliable_combinations)
 
 # required implication validity
 riv = 0.7
 
-reliable_combinations_stat = get_reliable_combinations_stat(combination_frequences, edible_counts, poisonous_counts, reliable_indexes, riv)
+# combination index - class - iv, the implication validity
+reliable_combinations_stat = get_reliable_combinations_stat(combination_frequences, edible_counts, poisonous_counts,
+reliable_indexes, riv)
 print(reliable_combinations_stat)
 
 
-
+# TODO:
+# compute weights for each implication in BZ where left side is a subcombination of the current combination
+# apply GLOB on those weights to get weight Wz of the combination
+# check the diff between calculated combination implication validity and the weight Wz from GLOB is true
+# then add implication to the BZ ?
+# else don`t
